3e00056b946aba6429e9d71e9919ef5047fb16e7
jtdelval <43562793+jtdelval@users.noreply.github.com>
2019-10-09 12:21:06 -0700

    02

--- test/d3-viz.js
+++ test/d3-viz.js
@@ -6,6 +6,8 @@ function getMaleApplied(data){
   return outputArray;
 }
 
+var dataset = d3.json("admissions.json").then(function(data){ console.log(data)});
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 var jj = d3.json('admissions.json');
 var fMA = jj.then(function(data) { return getMaleApplied(data);})
 
@@ -15,5 +17,20 @@ var barPadding = 5;
 var barWidth = svgWidth / 14
 var svg = d3.select('svg')
  .attr("width", svgWidth)
- .attr("height", svgHeight)
- .attr("class", "bar-chart");
+ .attr("height", svgHeight);
+
+var barChart = svg.selectAll('rect')
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("y", function(d) {
+    return svgHeight - d
+  })
+  .attr("height", function(d) {
+    return d;
+  })
+  .attr("width", barWidth - barPadding)
+  .attr("transform", function(d, i) {
+    var translate = [barWidth * i, 0];
+    return "translate(" + translate + ")";
+  });
